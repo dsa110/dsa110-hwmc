2b96925f24bb0917f35b62c753d43e0279858e6a
jwl-cit <54184836+jwl-cit@users.noreply.github.com>
2020-10-06 15:40:21 -0700

    Added attempt to download script if not load from memory unsuccessful. Modified logging method. Added logging.

--- hwmc/lj_startup.py
+++ hwmc/lj_startup.py
@@ -6,9 +6,18 @@ import inspect
 from labjack import ljm
 import dsautils.dsa_syslog as dsl
 from hwmc.common import Config as CONF
+import hwmc.lua_script_utilities as util
 
 
+# Set up module-level logging.
 MODULE_NAME = __name__
+LOGGER = dsl.DsaSyslogger(subsystem_name=CONF.SUBSYSTEM,
+                          log_level=CONF.LOGGING_LEVEL,
+                          logger_name=MODULE_NAME)
+LOGGER.app(CONF.APPLICATION)
+LOGGER.version(CONF.VERSION)
+LOGGER.level(CONF.LOGGING_LEVEL)
+LOGGER.info("{} logger created".format(MODULE_NAME))
 
 
 def t7_startup_check(lj_handle, lua_required, ant_num):
@@ -28,18 +37,11 @@ def t7_startup_check(lj_handle, lua_required, ant_num):
     # Set up class-level logging (per class instance).
     class_name = 'None'
     func = inspect.stack()[0][3]
-    print(func)
-    func_name = "{}".format(func)
-    logger_name = '{}_Ant{}'.format(MODULE_NAME, ant_num)
-    logger = dsl.DsaSyslogger(subsystem_name=CONF.SUBSYSTEM,
-                                   log_level=CONF.LOGGING_LEVEL,
-                                   logger_name=logger_name)
-    logger.app(CONF.APPLICATION)
-    logger.version(CONF.VERSION)
-    logger.function(func_name)
-    logger.info("{} logger created".format(logger_name))
-    logger.info("Initializing")
-    logger.info("Antenna {} connected".format(ant_num))
+    func_name = "{}::{}".format(MODULE_NAME, func)
+    LOGGER.app(CONF.APPLICATION)
+    LOGGER.version(CONF.VERSION)
+    LOGGER.function(func_name)
+    LOGGER.info("Labjack for Ant/BEB {} started".format(ant_num))
 
     start_up_state = dict(factory=False, prod_id=-1, hw_ver=0.0, fw_ver=0.0, boot_ver=0.0, ser_no=0,
                           dev_name='', lua_running=False, lua_code_ver=-1, config_valid=True)
@@ -62,17 +64,35 @@ def t7_startup_check(lj_handle, lua_required, ant_num):
     start_up_state['lua_running'] = bool(ljm.eReadName(lj_handle, 'LUA_RUN'))
     if start_up_state['lua_running'] is False and lua_required is True:
         print('Lua script not running. Attempting to load and start script')
+        LOGGER.info("Labjack for Ant/BEB {} Lua script not running. Attempting to load from memory"
+                    "".format(ant_num))
         ljm.eWriteName(lj_handle, 'LUA_LOAD_SAVED', 1)
         time.sleep(2.0)
         try:
             ljm.eWriteName(lj_handle, 'LUA_RUN', 1)
         except ljm.LJMError as e:
-            print(e)
+            LOGGER.critical("Labjack for Ant/BEB {} cannot load script".format(ant_num))
+            print("This is e: {}".format(e))
+            lua_script_name = CONF.LUA_DIR + "/antenna_control.lua"
+            LOGGER.critical("Attempting to download script {} to ant {}".format(lua_script_name,
+                                                                            ant_num))
+            script = util.LuaScriptUtilities(lua_script_name, lj_handle)
+            script.load()
+            script.save_to_flash()
+            script.run_on_startup()
+            script.run()
+            sleep(1)
+            if bool(ljm.eReadName(lj_handle, 'LUA_RUN')) is True:
+                LOGGER.critical("Success downloading script {} to ant {}".format(lua_script_name,
+                                                                                ant_num))
+            else:
+                LOGGER.critical("Failed to download script {} to ant {}".format(lua_script_name,
+                                                                                ant_num))
 
         time.sleep(2.0)
         start_up_state['lua_running'] = bool(ljm.eReadName(lj_handle, 'LUA_RUN'))
         if start_up_state['lua_running'] is False:
-            print('Failed to start script')
+            LOGGER.info("Labjack for Ant/BEB {} cannot run script".format(ant_num))
             start_up_state['config_valid'] = False
 
     start_up_state['lua_code_ver'] = format(float(ljm.eReadAddress(lj_handle, 46000, 3)), '.3f')
@@ -82,4 +102,5 @@ def t7_startup_check(lj_handle, lua_required, ant_num):
 
     if start_up_state['factory'] is False or start_up_state['prod_id'] != 7:
         start_up_state['config_valid'] = False
+    LOGGER.info("Logging Labjack for Ant/BEB {} startup done".format(ant_num))
     return start_up_state
