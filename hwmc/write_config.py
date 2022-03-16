f2f3f70b9b04b1db5ce7439fe3949f6d71720ad2
James Lamb <lamb@caltech.edu>
2022-03-16 13:11:11 -0700

    Added more logging for antenna cal.

--- hwmc/write_config.py
+++ hwmc/write_config.py
@@ -98,9 +98,12 @@ def write_config_to_flash(lj_handle, ant, cal_table):
             time.sleep(1.0)
             if ljm.eReadName(lj_handle, 'LUA_RUN') != 1:
                 LOGGER.error(f"Failed to restart Lua script after writing  Ant{ant} cal data.")
+                vprint(f"Failed to restart Lua script after writing  Ant{ant} cal data.")
             else:
                 LOGGER.message(f"Restarted Lua script after writing  Ant{ant} cal data.")
+                vprint(f"Restarted Lua script after writing  Ant{ant} cal data.")
         except ljm.LJMError:
             LOGGER.error(f"Failed to restart Lua script after writing Ant{ant} cal data.")
+            vprint(f"Failed to restart Lua script after writing Ant{ant} cal data.")
     else:
         LOGGER.info(f"Ant{ant} old inclinometer calibration values unchanged")
