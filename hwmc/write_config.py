767e4a8563e5ad3028d4fc4bfb816ade826f7e16
James Lamb <lamb@caltech.edu>
2022-03-30 14:36:33 -0700

    Tidied up some debug print statements. Made formatting more consistent to enable easier searching of debugging messages.

--- hwmc/write_config.py
+++ hwmc/write_config.py
@@ -97,13 +97,13 @@ def write_config_to_flash(lj_handle, ant, cal_table):
             ljm.eWriteName(lj_handle, 'LUA_RUN', 1)
             time.sleep(1.0)
             if ljm.eReadName(lj_handle, 'LUA_RUN') != 1:
-                LOGGER.error(f"Failed to restart Lua script after writing Ant{ant} cal data.")
-                vprint(f"Failed to restart Lua script after writing Ant{ant} cal data.")
+                LOGGER.error(f"Failed to restart Lua script after writing Ant-{ant} cal data.")
+                vprint(f"Failed to restart Lua script after writing Ant-{ant} cal data.")
             else:
-                LOGGER.info(f"Restarted Lua script after writing Ant{ant} cal data.")
-                vprint(f"Restarted Lua script after writing Ant{ant} cal data.")
+                LOGGER.info(f"Restarted Lua script after writing Ant-{ant} cal data.")
+                vprint(f"Restarted Lua script after writing Ant-{ant} cal data.")
         except ljm.LJMError:
-            LOGGER.error(f"Failed to restart Lua script after writing Ant{ant} cal data.")
-            vprint(f"Failed to restart Lua script after writing Ant{ant} cal data.")
+            LOGGER.error(f"Failed to restart Lua script after writing Ant-{ant} cal data.")
+            vprint(f"Failed to restart Lua script after writing Ant-{ant} cal data.")
     else:
-        LOGGER.info(f"Ant{ant} old inclinometer calibration values unchanged")
+        LOGGER.info(f"Ant-{ant} old inclinometer calibration values unchanged")
